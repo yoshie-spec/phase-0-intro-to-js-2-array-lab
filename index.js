70aaa6528d504c8659d3497d2b792ab0d9c1f06c
yosselin alvarez <yoshie_9311@icloud.com>
2023-04-13 08:22:49 -0400

    complete lab' end exit
    end
    cd

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+    return cats.push;
+}
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+    return cats;
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat() {
+    const moreCats = [...cats, "Broom"];  
+        return moreCats;
+}
+
+function prependCat(){
+    const moreCats= ["Arnold", ...cats]
+    return moreCats;
+}
+
+function removeLastCat(){
+const moreCats = cats.slice()
+moreCats.pop()
+return moreCats;
+}
+
+
+function removeFirstCat(){
+    const moreCats = cats.slice()
+    moreCats.shift()
+    return moreCats;
+}
